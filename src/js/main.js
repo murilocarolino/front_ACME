fa7420a9e6594f0d5a2e91318c57331f27728e64
MURILO CAROLINO DOS SANTOS <23111821@CFP127ALUNO.LOCAL>
2024-04-11 17:11:56 -0300

    front

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,17 @@
 
 import {getFilmes, getFilme, postFilme, putFilme} from "./filmes.js"
 
-function criarCardFilme (filme) {
-    const card = document.createElement('div')
-    const containerFilme = document.getElementById('containerFilme')
-    containerFilme.classList.add(
-        'flex', 
-        'gap-4',
-        'p-10',
-    )
 
+async function preencherContainerSerie () {
+    const containerSerie = document.getElementById('containerSerie')
+
+    const filme = await getFilmes ()
+    const titulo = document.createElement ('h1')
+    titulo.textContent = "Serie"
+
+    filme.forEach (filme => {
+        const card = document.createElement('div')
+        const containerFilme = document.getElementById('containerSerie')
     // const titulo = document.createElement('h2')
     // titulo.textContent = filme.nome
 
@@ -22,27 +24,28 @@ function criarCardFilme (filme) {
     capa.classList.add(
         'border-vermelho',
         'border-4',
-        'h-250',
-        'w-100',
+        'h-full',
+        'min-w-72',
         'rounded-md',
         'gap-y-10',
     )
 
     card.appendChild(capa)
-    containerFilme.appendChild(card)
-
-    return containerFilme
+        containerFilme.appendChild(card)
+        console.log (card)
+    })
 }
 
-function criarCardSerie (serie) {
-    const card = document.createElement('div')
-    const containerSerie = document.getElementById('containerSerie')
-    containerSerie.classList.add(
-        'flex', 
-        'gap-4',
-        'p-10',
-    )
+async function preencherContainerFilme () {
+    const containerFilme = document.getElementById('containerFilme')
 
+    const filme = await getFilmes ()
+    const titulo = document.createElement ('h1')
+    titulo.textContent = "Filme"
+
+    filme.forEach (filme => {
+        const card = document.createElement('div')
+        const containerFilme = document.getElementById('containerFilme')
     // const titulo = document.createElement('h2')
     // titulo.textContent = filme.nome
 
@@ -50,44 +53,18 @@ function criarCardSerie (serie) {
     // texto.textContent = filme.sinopse
 
     const capa = document.createElement ('img')
-    capa.src = serie.foto_capa
+    capa.src = filme.foto_capa
     capa.classList.add(
         'border-vermelho',
         'border-4',
-        'h-250',
-        'w-100',
+        'h-full',
+        'min-w-56',
         'rounded-md',
         'gap-y-10',
     )
 
     card.appendChild(capa)
-    containerSerie.appendChild(card)
-
-    return containerSerie
-}
-
-async function preencherContainerFilme () {
-    const containerFilme = document.querySelector ('body')
-
-    const filme = await getFilmes ()
-    const titulo = document.createElement ('h1')
-    titulo.textContent = "Filme"
-
-    filme.forEach (filme => {
-        const card = criarCardFilme (filme)
         containerFilme.appendChild(card)
-        console.log (card)
-    })
-}
-
-async function preencherContainerSerie () {
-    const containerSerie = document.querySelector ('body')
-
-    const serie = await getFilmes ()
-
-    serie.forEach (serie => {
-        const card = criarCardSerie (serie)
-        containerSerie.appendChild(card)
         console.log (card)
     })
 }
